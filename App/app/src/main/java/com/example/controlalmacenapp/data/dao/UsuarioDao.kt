3dd51ab31a003f40a54862e218981597927799d6
Xabier Oñederra <xabier.onederrada@elorrieta-errekamari.com>
2026-03-06 12:33:35 +0100

    dao y database

--- App/app/src/main/java/com/example/controlalmacenapp/data/dao/UsuarioDao.kt
+++ App/app/src/main/java/com/example/controlalmacenapp/data/dao/UsuarioDao.kt
@@ -1,4 +1,25 @@
 package com.example.controlalmacenapp.data.dao
 
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.controlalmacenapp.data.entidades.UsuarioEntity
+
 interface UsuarioDao {
+    @Insert( onConflict = OnConflictStrategy.ABORT)
+    suspend fun insertUsuario(usuario: UsuarioEntity): Long
+
+    @Insert( onConflict = OnConflictStrategy.ABORT)
+    suspend fun insertUsuarioLista(usuarios: List <UsuarioEntity>):List <Long>
+
+    @Update
+    suspend fun updateUsuario(usuario: UsuarioEntity): Int
+
+    @Delete
+    suspend fun deleteUsuario(usuario: UsuarioEntity): Int
+
+    @Query("SELECT * FROM UsuarioEntity")
+    suspend fun getUsuarios():List<UsuarioEntity>
 }
--- App/app/src/main/java/com/example/controlalmacenapp/data/database/AppDatabase.kt
+++ App/app/src/main/java/com/example/controlalmacenapp/data/database/AppDatabase.kt
@@ -1,4 +1,30 @@
 package com.example.controlalmacenapp.data.database
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.controlalmacenapp.data.entidades.ProductoEntity
+import com.example.controlalmacenapp.data.entidades.UsuarioEntity
+
+@Database(entities = [UsuarioEntity::class, ProductoEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase(){
+    //Singleton pattern
+    companion object {
+        private var instance: AppDatabase? = null
+
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: buildDatabase(context).also { instance = it }
+        }
+
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "myDataase"
+        )
+            .build()
+
+    }
 }
