5a03b39265f9877eadd733616d5b0e83a8a30147
Xabier Oñederra <xabier.onederrada@elorrieta-errekamari.com>
2026-03-22 12:18:40 +0100

    insert

--- App/app/src/main/java/com/example/controlalmacenapp/view/NuevoUsuarioActivity.kt
+++ App/app/src/main/java/com/example/controlalmacenapp/view/NuevoUsuarioActivity.kt
@@ -1,12 +1,14 @@
 package com.example.controlalmacenapp.view
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
-import android.widget.ImageView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.controlalmacenapp.R
+import com.example.controlalmacenapp.model.database.AppDatabase
+import com.example.controlalmacenapp.model.entities.UsuarioEntity
+import kotlinx.coroutines.launch
 
 class NuevoUsuarioActivity : AppCompatActivity() {
 
@@ -15,21 +17,43 @@ class NuevoUsuarioActivity : AppCompatActivity() {
     private lateinit var cbAdmin: CheckBox
     private lateinit var etPassword: EditText
     private lateinit var etEmail: EditText
-    private lateinit var btnEditar: Button
-    private lateinit var btnEliminar: Button
+    private lateinit var btnGuardar: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nuevo_usuario)
-
 
         ivFoto = findViewById(R.id.ivFoto)
         etNombre = findViewById(R.id.etNombre)
         cbAdmin = findViewById(R.id.cbAdmin)
         etPassword = findViewById(R.id.etPassword)
         etEmail = findViewById(R.id.etEmail)
-        btnEditar = findViewById(R.id.btnEditar)
-        btnEliminar = findViewById(R.id.btnEliminar)
+        btnGuardar = findViewById(R.id.btnGuardar)
 
+        val db = AppDatabase(this)
+        val usuarioDao = db.usuarioDao()
+
+        btnGuardar.setOnClickListener {
+
+            val nombre = etNombre.text.toString()
+            val password = etPassword.text.toString()
+            val email = etEmail.text.toString()
+            val admin = cbAdmin.isChecked
+
+            val user = UsuarioEntity(
+                nombre = nombre,
+                password = password,
+                email = email,
+                esAdministrador = admin,
+                habilitado = true,
+                imagenUrl = ""
+            )
+
+            lifecycleScope.launch {
+                usuarioDao.insertUsuario(user)
+            }
+            startActivity(Intent(this@NuevoUsuarioActivity, MainActivity::class.java))
+            finish()
+        }
     }
 }
